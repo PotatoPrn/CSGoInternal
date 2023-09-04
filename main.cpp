e08cc191a3f7174601e1df1426c6c45a7e25e282
Potatech <potatech.geneva@dralias.com>
2023-09-04 20:52:04 +1000

    Player Entity Check

--- main.cpp
+++ main.cpp
@@ -31,8 +31,12 @@ void InitHack(HMODULE hModule)
 
 	LocateOffsets();
 
+
 	// Setup Player Entity
-	HackClass.PlayerEntity = *(PlayerObject**)(HackClass.ClientBase + PresetOffset::dwLocalPlayer);
+	do
+	{
+		HackClass.PlayerEntity = *(PlayerObject**)(HackClass.ClientBase + PresetOffset::dwLocalPlayer);
+	} while (HackClass.PlayerEntity == NULL);
 
 	// Setup Console
 	UI::ClearConsole();
@@ -42,7 +46,8 @@ void InitHack(HMODULE hModule)
 
 
 	// Call HackThread
-	HackThread(HackClass);
+	HackThread();
+
 
 	// Eject DLL
 	// May need a cleanup thread here...
@@ -71,11 +76,10 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 
 
 // Main HackThread
-void HackThread(GameHack HackClass)
+void HackThread()
 {
 	while (true)
 	{
-
 
 		/// DLL Detach Hotkey
 		if (GetAsyncKeyState(VK_DELETE) & 1)
@@ -84,18 +88,11 @@ void HackThread(GameHack HackClass)
 		}
 
 
-		// Using interface to retrieve the entity list
-		// Wouldnt use in an actual cheat as it's commonly used in the sdk...
-
-
-
-
-		/// BHop Related Hack Toggle
-		if (GetAsyncKeyState(VK_F9) & 1)
+		/// Aimbot Hack Toggle
+		if (GetAsyncKeyState(VK_F7) & 1)
 		{
-			THacks.T_BHop = !THacks.T_BHop;
+			THacks.T_AimBot = !THacks.T_AimBot;
 		}
-
 
 		/// Glow Hack Toggle
 		if (GetAsyncKeyState(VK_F8) & 1)
@@ -103,6 +100,13 @@ void HackThread(GameHack HackClass)
 			GlowUtils::ModifyBrightness();
 			THacks.T_Glow = !THacks.T_Glow;
 		}
+
+		/// BHop Related Hack Toggle
+		if (GetAsyncKeyState(VK_F9) & 1)
+		{
+			THacks.T_BHop = !THacks.T_BHop;
+		}
+
 
 		/// Triggerbot Hack Toggle
 		if (GetAsyncKeyState(VK_F10) & 1)
@@ -123,11 +127,7 @@ void HackThread(GameHack HackClass)
 		}
 
 
-		/// Aimbot Hack Toggle
-		if (GetAsyncKeyState(VK_F7) & 1)
-		{
-			THacks.T_AimBot = !THacks.T_AimBot;
-		}
+
 
 
 
@@ -154,7 +154,7 @@ void HackThread(GameHack HackClass)
 
 
 		// Toggled Hacks Cout
-		std::cout << "[F7] Aimbot Hack > " << THacks.T_BHop << std::endl;
+		std::cout << "[F7] Aimbot Hack > " << THacks.T_AimBot << std::endl;
 		std::cout << "[F8] Glow Hack > " << THacks.T_Glow << std::endl;
 		std::cout << "[F9] Bhop Hack > " << THacks.T_BHop << std::endl;
 		std::cout << "[F10] / [Shift] Triggerbot Hack > " << THacks.T_TrigBot << std::endl;
